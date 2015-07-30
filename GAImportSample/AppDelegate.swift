72b290c6e498d046249ccc56170010adb38cc672
mogmet <mogmet@gmail.com>
2015-07-30 21:26:37 +0900

    build successed

--- GAImportSample/AppDelegate.swift
+++ GAImportSample/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleAnalytics_iOS_SDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let hoge = GAI.sharedInstance()
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-GoogleAnalytics/Pods-GoogleAnalytics-umbrella.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+#import "GAI.h"
+#import "GAIDictionaryBuilder.h"
+#import "GAIEcommerceFields.h"
+#import "GAIEcommerceProduct.h"
+#import "GAIEcommerceProductAction.h"
+#import "GAIEcommercePromotion.h"
+#import "GAIFields.h"
+#import "GAILogger.h"
+#import "GAITrackedViewController.h"
+#import "GAITracker.h"
+
+FOUNDATION_EXPORT double GoogleAnalyticsVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleAnalyticsVersionString[];
+
